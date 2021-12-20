9a324344334df4540cd375a962c9ebb6ea48b487
Wytai-Scarab <82951681+Wytai-Scarab@users.noreply.github.com>
2021-12-20 15:14:17 +0000

    Initial Customisations

--- 2-user.sh
+++ 2-user.sh
@@ -25,7 +25,7 @@ ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
 PKGS=(
 'autojump'
 'awesome-terminal-fonts'
-'brave-bin' # Brave Browser
+'firedragon' # Firedragon Browser
 'dxvk-bin' # DXVK DirectX to Vulcan
 'github-desktop-bin' # Github Desktop sync
 'lightly-git'
@@ -49,16 +49,80 @@ PKGS=(
 'ttf-roboto'
 'zoom' # video conferences
 'snap-pac'
+'beautyline'
+'candy-icons-git'
+'sweet-kde-git'
+'sweet-gtk-theme-dark'
+'sweet-gtk-theme'
+'kvantum-theme-sweet-git'
+'sweet-theme-git'
+'xcursor-sweet'
+'plasma5-theme-sweet-git'
+'lutris'
+'steam'
+'steam-native-runtime'
+'protonup-git'
+'protonup-qt'
+'boxtron'
+'qbittorrent'
+'inkscape'
+'libreoffice-fresh'
+'spotify'
+'zettlr-bin'
+'flameshot'
+'discord'
+'noisetorch'
+'fish'
+'starship'
+'samba'
+'minecraft'
+'latte-dock-git'
+'zotero-git'
+'stacer'
+'whatsapp-nativefier'
+'mcfly'
+'bat'
+'lsd'
+'bitwarden'
+'vlc'
+'mpv'
+'unified-remote-server'
+'zeit'
+'cht.sh-git'
+'zerotier-one'
+'clonezilla'
+'zoxide'
+'realvnc-vnc-viewer'
+'realvnc-vnc-server'
+'speedtest-cli'
+'mc'
+'ncdu'
+'tmux'
+'simplehttp'
+'ntop'
+'bpytop'
+'aria2'
+'ncspot'
+'ranger'
+'fd'
+'fzf'
+'hyperfine'
+'gping'
+'curlie'
+'balena-etcher'
+'hunspell'
+'garuda-wallpapers'
+'appimagelauncher'
 )
 
 for PKG in "${PKGS[@]}"; do
-    yay -S --noconfirm $PKG
+    yay -S --noconfirm --needed $PKG
 done
 
 export PATH=$PATH:~/.local/bin
-cp -r $HOME/ArchTitus/dotfiles/* $HOME/.config/
+cp -r $HOME/ArchAetos/dotfiles/* $HOME/.config/
 pip install konsave
-konsave -i $HOME/ArchTitus/kde.knsv
+konsave -i $HOME/ArchAetos/kde.knsv
 sleep 1
 konsave -a kde
 
